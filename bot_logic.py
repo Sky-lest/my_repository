a7e124642c3243d93d4ba2c1f3aa43461b86d5c8
Sky-lest <henriquepematos08@gmail.com>
2026-07-26 20:19:17 -0300

    Add coin flip, emoji generation, and yes/no functions
    Added functions to flip a coin, generate emojis, and return yes/no responses.

--- bot_logic.py
+++ bot_logic.py
@@ -6,4 +6,21 @@ def gen_pass(pass_length):
     for i in range(pass_length):
         password += random.choice(elements)
     return password
-    
+
+def flip_coin():
+    flip = random.randint(0, 1)
+    if flip == 0:
+        return "cara"
+    else:
+        return "coroa"
+
+def gen_emodji():
+    emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    return random.choice(emodji)
+
+def yes_no():
+    choice = random.randint(0, 1)
+    if choice == 0:
+        return 'Sim'
+    else:
+        return 'Não'
