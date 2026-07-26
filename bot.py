d8e55798f617c8ca973b4732346524263b78346a
Sky-lest <henriquepematos08@gmail.com>
2026-07-26 20:18:46 -0300

    Add new commands for coin flip, emoji, and choice

--- bot.py
+++ bot.py
@@ -1,5 +1,10 @@
 import discord
+from bot_settings import settings
 from bot_logic import gen_pass
+from bot_logic import flip_coin
+from bot_logic import gen_emodji
+from bot_logic import yes_no
+
 
 # A variável intents armazena as permissões do bot
 intents = discord.Intents.default()
@@ -22,7 +27,13 @@ async def on_message(message):
         await message.channel.send("\U0001f642")
     elif message.content.startswith('$password'):
             await message.channel.send(gen_pass(10))
+    elif message.content.startswith('$coin'):
+            await message.channel.send(flip_coin())
+    elif message.content.startswith('$emoji'):
+                await message.channel.send(gen_emodji())
+    elif message.content.startswith('$choice'):
+                await message.channel.send(yes_no())
     else:
-        await message.channel.send(message.content)
+        await message.channel.send("Comando não encontrado")
 
-client.run("MTUyODg5OTMwMTc2ODU2MDg1MA.GyEwOI.7HDA-lvuQeZB_jULkQJGJjoROZcOffh1mglJb8")
+client.run(settings['TOKEN'])
